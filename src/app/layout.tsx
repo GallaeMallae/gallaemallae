b72397aefe71c33593c1906e34a8f172598efac1
soundraven <kin9709@gmail.com>
2026-03-03 16:15:04 +0900

    Design: 폰트 설정 & 디자인 토큰 작업 결과물 브랜치에 반영

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { pretendard } from "./fonts/fonts";
 import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
 import AuthProvider from "@/components/providers/AuthProvider";
 import { Toaster } from "@/components/ui/sonner";
 import ProfileInitializer from "@/components/ProfileInitializer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "갈래말래",
@@ -27,10 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="ko">
+      <body className={`${pretendard.variable} font-sans`}>
         <ReactQueryProvider>
           <AuthProvider>
             <ProfileInitializer />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,34 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="p-4">
-      <h1>갈래말래</h1>
-      <Button>Go</Button>
+      <p>갈래말래</p>
+      <p className="font-semibold">갈래말래 세미볼드</p>
+      <p className="font-bold">갈래말래 볼드</p>
+
+      <br />
+
+      {/* Headings 클래스명 작성 X - 태그만 써도 됨 */}
+      <h1>갈래말래 h1</h1>
+      <h2>갈래말래 h2</h2>
+      <h3>갈래말래 h3</h3>
+
+      {/* Body 클래스명 작성 O  */}
+      <p className="text-title1">갈래말래 타이틀1</p>
+      <p className="text-title2">갈래말래 타이틀2</p>
+      <p className="text-desc1">갈래말래 본문1</p>
+      <p className="text-desc2">갈래말래 본문1</p>
+      <p className="text-caption">갈래말래</p>
+
+      <br />
+
+      <div className="flex gap-2">
+        <Button className="bg-symbol-sky-sub text-symbol-sky">Go</Button>
+        <Button className="bg-symbol-brown">Go</Button>
+        <Button className="bg-festival-sub text-festival">축제</Button>
+        <Button className="bg-performance-sub text-performance">공연</Button>
+        <Button className="bg-exhibition-sub text-exhibition">전시</Button>
+        <Button className="bg-etc-sub text-etc">기타</Button>
+      </div>
     </div>
   );
 }
