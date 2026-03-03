0d4ecc703a621fafca3dfae9923109b62b28c1d0
soundraven <kin9709@gmail.com>
2026-03-03 11:48:47 +0900

    Design: 로그인 페이지 이미지 불꽃놀이 사진 임시로 배정, 스타일 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,22 +25,26 @@ export default function LoginPage() {
 
   return (
     <div className="flex min-h-screen flex-col md:flex-row">
-      <div className="relative h-[50vh] w-full bg-red-500 md:h-full md:min-h-screen md:w-1/2">
-        이미지
+      <div className="relative min-h-[50vh] w-full md:h-full md:min-h-screen md:w-1/2">
+        <Image
+          src="/images/login-page-sparkle-image.jpg"
+          alt="Login page Image"
+          fill
+          className="object-cover"
+          priority
+        />
       </div>
-      <div className="flex h-[50vh] w-full flex-col items-center justify-around px-4 py-12 md:h-screen md:w-1/2 md:px-8">
+      <div className="flex min-h-[50vh] w-full flex-col items-center justify-center gap-15 px-4 py-12 md:h-screen md:w-1/2 md:px-8">
         <div className="flex flex-col items-center justify-center gap-4">
-          <div className="cursor-pointer">
-            <Link href={"/"}>
-              <Image
-                src="/images/gallaemallae-full-logo.svg"
-                alt="Gallaemallae Full Logo"
-                width={200}
-                height={60}
-                className="h-auto w-auto"
-              />
-            </Link>
-          </div>
+          <Link href={"/"} className="cursor-pointer">
+            <Image
+              src="/images/gallaemallae-full-logo.svg"
+              alt="Gallaemallae Full Logo"
+              width={200}
+              height={60}
+              className="h-20 w-auto md:h-24"
+            />
+          </Link>
           <div className="text-muted-foreground">
             갈래말래를 통해 위치, 날씨 기반 행사 추천 서비스를 만나보세요!
           </div>
@@ -49,32 +53,30 @@ export default function LoginPage() {
         <div className="flex w-full flex-col gap-2">
           <Button
             variant="outline"
-            className="flex h-12 w-full cursor-pointer items-center justify-center gap-2 border-[#dadce0] bg-white px-4 text-base text-[#3c4043] hover:bg-[#f8f9fa] hover:text-[#3c4043]"
+            className="border-[#747775 ] text-[#1F1F1F ] flex h-12 w-full cursor-pointer items-center justify-center gap-2 bg-white px-4 text-base hover:bg-[#f8f9fa]"
             onClick={() => signInWithOAuthAction("google")}
           >
             <Image
               src="/images/google-logo.svg"
               alt="Google Logo"
-              width={24}
-              height={24}
+              width={20}
+              height={20}
             />
             구글로 계속하기
           </Button>
           <Button
-            className="flex h-12 w-full cursor-pointer items-center justify-center gap-2 border-none bg-[#FEE500] px-4 text-base text-[#191919] hover:bg-[#FADA00]"
+            className="flex h-12 w-full cursor-pointer items-center justify-center gap-2 border-none bg-[#FEE500] px-4 text-base text-black hover:bg-[#FADA00]"
             onClick={() => signInWithOAuthAction("kakao")}
           >
             <Image
               src="/images/kakao-logo.svg"
               alt="Kakao Logo"
-              width={24}
-              height={24}
+              width={20}
+              height={20}
             />
             카카오로 계속하기
           </Button>
         </div>
-
-        <div className="text-muted-foreground">이용약관 | 개인정보처리방침</div>
       </div>
     </div>
   );
