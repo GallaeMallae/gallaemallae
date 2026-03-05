3db7918134729e60c69e3ae72a6d8fbd79c7cfc3
uzzin <01uzzin@gmail.com>
2026-03-05 16:01:46 +0900

    Feat: 메인 배너 UI 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
 "use client";
 
+import MainBanner from "@/components/home/MainBanner";
 import CategoryMenu from "@/components/home/CategoryMenu";
 import UpcomingEvents from "@/components/home/UpcomingEvents";
 import { useState } from "react";
-import { PeriodFilter } from "@/types/common";
+import { WeatherCardItem, RecommendType, PeriodFilter } from "@/types/common";
 import { MOCK_EVENTS } from "@/mocks/events";
 
 export default function Home() {
   const [selectedPeriodTab, setSelectedPeriodTab] =
     useState<PeriodFilter>("전체");
 
+  const weatherData: WeatherCardItem = {
+    weatherType: "cloudy",
+    location: "수원시 권선구",
+    temperature: 4,
+    fineDust: "좋음",
+    ultrafineDust: "보통",
+  };
+
+  const recommendType: RecommendType = "positive";
+
   return (
     <div className="flex flex-col gap-8">
+      <MainBanner weather={weatherData} recommendType={recommendType} />
       <CategoryMenu />
       <UpcomingEvents
         events={MOCK_EVENTS}
