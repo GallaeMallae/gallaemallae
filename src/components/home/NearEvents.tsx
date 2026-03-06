5c5fe680412214f6dab998575e370a63eb269f52
이유진 <156034560+uzzini@users.noreply.github.com>
2026-03-06 13:37:20 +0900

    #7 Feat: 메인 페이지 퍼블리싱 작업 (#8)
    * Design: 레이아웃 공통 여백 및 최대 너비 설정
    
    * Feat: 카테고리 검색 UI 구현
    
    * Feat: 다가오는 행사 UI 구현
    
    * Design: 행사 카드 타이틀 줄바꿈 대비
    
    * Feat: 메인 배너 UI 구현
    
    * Feat: 내 근처 행사 UI 구현
    
    * Design: 카드 컴포넌트 border + shadow 디자인 통일

---
+++ src/components/home/NearEvents.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Card, CardContent } from "../ui/card";
+import EventCard from "@/components/common/EventCard";
+import MoreCard from "@/components/common/MoreCard";
+import { EventCardItem } from "@/types/common";
+
+export default function NearEvents({ events }: { events: EventCardItem[] }) {
+  return (
+    <section>
+      <h3 className="text-title2 mb-2 font-bold">내 근처 행사</h3>
+
+      <div className="flex flex-col gap-4 md:h-110 md:flex-row">
+        <div className="bg-etc-sub h-56 w-full overflow-hidden rounded-xl md:h-full md:flex-[0.4]">
+          {/* Todo: Kakao Map 연결 */}
+          <div className="text-etc flex h-full w-full items-center justify-center">
+            Map Area
+          </div>
+        </div>
+
+        <div className="md:flex-[0.6]">
+          <div className="flex flex-col gap-4 md:h-full md:overflow-y-auto md:pr-2 md:pb-2">
+            {events.map((event, index) => (
+              <EventCard key={index} {...event} />
+            ))}
+
+            <div className="hidden md:block">
+              <MoreCard />
+            </div>
+
+            {/* 모바일 더보기 */}
+            <div className="md:hidden">
+              <Card className="cursor-pointer rounded-2xl">
+                <CardContent className="flex items-center justify-center">
+                  <span className="text-caption text-symbol-sky font-bold">
+                    더보기
+                  </span>
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
