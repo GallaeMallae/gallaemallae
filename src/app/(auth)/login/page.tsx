f18fa7926160356f97692b57ba68bed9ae2a53ba
soundraven <kin9709@gmail.com>
2026-03-09 18:01:40 +0900

    Fix: Suspense 이용하여 빌드 에러 해결

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,14 +4,14 @@ import { Button } from "@/components/ui/button";
 import { signInWithOAuthAction } from "@/actions/auth/signInWithOAuthAction";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { toast } from "sonner";
 import Image from "next/image";
 import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
 import { ERROR_MESSAGE_CONFIG } from "@/lib/constants";
 
-export default function LoginPage() {
+function ErrorToastHandler() {
   const searchParams = useSearchParams();
   const router = useRouter();
   const error = searchParams.get("error");
@@ -21,16 +21,21 @@ export default function LoginPage() {
       toast.error(
         ERROR_MESSAGE_CONFIG[error] ?? "로그인 중 오류가 발생했습니다.",
       );
+
       const params = new URLSearchParams(searchParams.toString());
       params.delete("error");
-
       const nextQuery = params.toString();
+
       router.replace(nextQuery ? `/login?${nextQuery}` : "/login", {
         scroll: false,
       });
     }
   }, [error, searchParams, router]);
 
+  return null;
+}
+
+export default function LoginPage() {
   return (
     <div className="flex flex-1 flex-col md:flex-row">
       <div className="relative hidden flex-1 md:block md:w-1/2">
@@ -91,6 +96,11 @@ export default function LoginPage() {
           <Link href={"/"}>개인정보처리방침</Link>
         </div>
       </div>
+      {/* useSearchParams 때문에 빌드 시점에 정적으로 페이지를 만들 수가 없음
+          Suspense로 감싸서 파라미터 읽어올 때까지 렌더링 미루도록 하여 해결 */}
+      <Suspense fallback={null}>
+        <ErrorToastHandler />
+      </Suspense>
     </div>
   );
 }
