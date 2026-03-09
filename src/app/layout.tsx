aa36a46443df194a0a2f6d785204871095d7bdac
uzzin <01uzzin@gmail.com>
2026-03-09 16:24:16 +0900

    Refactor: RootLayout MainLayout 구조 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { pretendard } from "./fonts/fonts";
 import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
-import Header from "@/components/common/Header";
-import Footer from "@/components/common/Footer";
-import MobileBottomNav from "@/components/common/MobileBottomNav";
-import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "갈래말래",
@@ -19,17 +15,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${pretendard.variable} bg-background-base pb-16 font-sans md:pb-0`}
-      >
-        <ReactQueryProvider>
-          <Header />
-          <main className="mx-auto max-w-7xl p-6">{children}</main>
-          <Footer />
-          <Suspense fallback={null}>
-            <MobileBottomNav />
-          </Suspense>
-        </ReactQueryProvider>
+      <body className={`${pretendard.variable} bg-background-base font-sans`}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
   );
