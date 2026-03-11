95062a2052ee56994b8dd9a9e0f79df1e392571a
GwonWooL <229861@naver.com>
2026-03-12 02:49:07 +0900

    Feat: 이벤트 상세 모달 액션 버튼(EventDetailModalButton) 컴포넌트 구현

---
+++ src/components/map/EventDetailModal/EventDetailModalRight/EventDetailButton.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Heart } from "lucide-react";
+export default function EventDetailModalButton() {
+  return (
+    <div className="flex gap-4">
+      <Button className="bg-symbol-sky text-desc1! flex h-12 flex-1 text-white">
+        나의 일정에 추가하기
+        <ArrowRight size={20} />
+      </Button>
+
+      <Button size="icon" variant="ghost">
+        <Heart size={24} className="text-slate-300" />
+      </Button>
+    </div>
+  );
+}
