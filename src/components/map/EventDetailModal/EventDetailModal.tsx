c3a7dd465c6e9477e7d011f36d7603752df0fea5
GwonWooL <229861@naver.com>
2026-03-12 02:47:22 +0900

    Feat: 이벤트 상세 모달 컴포넌트 구현

---
+++ src/components/map/EventDetailModal/EventDetailModal.tsx
@@ -0,0 +1,26 @@
+import EventDetailModalLeft from "@/components/map/EventDetailModal/EventDetailModalLeft/EventDetailModalLeft";
+import EventDetailModalRight from "@/components/map/EventDetailModal/EventDetailModalRight/EventDetailModalRight";
+
+import { Badge } from "@/components/ui/badge";
+import { X } from "lucide-react";
+import { Button } from "@/components/ui/button";
+export default function EventDetailModal() {
+  return (
+    <div className="flex flex-col gap-6 p-8">
+      <div className="flex items-start justify-between">
+        <div className="flex flex-col gap-2">
+          <Badge variant="축제">축제</Badge>
+          <p className="text-h2">서울 국제 불꽃 축제</p>
+        </div>
+        <Button>
+          <X />
+        </Button>
+      </div>
+
+      <div className="flex flex-col gap-6 md:flex-row">
+        <EventDetailModalLeft />
+        <EventDetailModalRight />
+      </div>
+    </div>
+  );
+}
