c7d2bdab5e1c1db1002daf66e333e5aa6c51b58b
GwonWooL <229861@naver.com>
2026-03-16 10:09:48 +0900

    Feat: 지도 페이지에 카테고리 상태 추가 및 Sidebar, Area에 전달

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -3,21 +3,25 @@
 import Sidebar from "@/components/map/Sidebar/Sidebar";
 import Area from "@/components/map/Area/Area";
 import { useState } from "react";
+import { Category } from "@/types/common";
 
 export default function MapPage() {
   const [radius, setRadius] = useState<number | null>(null);
+  const [category, setCategory] = useState<Category>("all");
 
   return (
     <div className="relative w-full overflow-hidden md:flex">
-      <div className="hidden md:block md:w-90 md:shrink-0">
-        <Sidebar radius={radius} setRadius={setRadius} />
+      <div className="absolute top-0 left-0 z-20 w-full md:relative md:block md:w-90 md:shrink-0">
+        <Sidebar
+          radius={radius}
+          setRadius={setRadius}
+          category={category}
+          setCategory={setCategory}
+        />
       </div>
 
       <div className="flex-1">
-        <Area radius={radius} />
-      </div>
-      <div className="absolute top-0 left-0 z-20 w-full md:hidden">
-        <Sidebar radius={radius} setRadius={setRadius} />
+        <Area radius={radius} category={category} />
       </div>
     </div>
   );
