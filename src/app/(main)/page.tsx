e2501aec4a137bc0065419aed748a0615fe0eba0
uzzin <01uzzin@gmail.com>
2026-03-16 14:24:59 +0900

    Refactor: Home 컴포넌트에서 사용하지 않는 useInitLocation 호출 제거

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,7 +6,6 @@ import UpcomingEvents from "@/components/home/UpcomingEvents";
 import NearEvents from "@/components/home/NearEvents";
 import { useState } from "react";
 import { useLocationStore } from "@/stores/locationStore";
-import { useInitLocation } from "@/hooks/useInitLocation";
 import { useWeatherData } from "@/hooks/queries/useWeatherData";
 import { useLocationNameData } from "@/hooks/queries/useLocationNameData";
 import { useAirPollutionData } from "@/hooks/queries/useAirPollutionData";
@@ -16,8 +15,6 @@ import { PeriodFilter } from "@/types/common";
 import { MOCK_EVENTS } from "@/mocks/events";
 
 export default function Home() {
-  useInitLocation();
-
   const { coords, isInitialized, isDefaultLocation } = useLocationStore();
   const { data: locationNameData } = useLocationNameData(coords, isInitialized);
   const { data: weatherData } = useWeatherData(coords, isInitialized);
