d265e5ae7100ee5dff9d22ec785d4eb8390e13e0
GwonWooL <229861@naver.com>
2026-03-17 15:53:36 +0900

    Feat: 이벤트 상세 정보 모달 메인 컴포넌트 구현

--- src/components/map/EventDetailModal/EventDetailModal.tsx
+++ src/components/map/EventDetailModal/EventDetailModal.tsx
@@ -4,22 +4,51 @@ import { Badge } from "@/components/ui/badge";
 import { X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-export default function EventDetailModal() {
+interface EventData {
+  categories: string[];
+  created_at: string | null;
+  data_reference_date: string | null;
+  description: string | null;
+  end_date: string;
+  homepage_url: string | null;
+  host: string | null;
+  id: string;
+  latitude: number | null;
+  longitude: number | null;
+  lot_address: string | null;
+  name: string;
+  organizer: string | null;
+  phone: string | null;
+  provider: string | null;
+  related_info: string | null;
+  road_address: string | null;
+  sponsor: string | null;
+  start_date: string;
+  updated_at: string | null;
+  venue: string | null;
+}
+
+type Props = {
+  event: EventData;
+  onClose: () => void;
+};
+
+export default function EventDetailModal({ event, onClose }: Props) {
   return (
     <div className="flex flex-col gap-6 p-8">
       <div className="flex items-start justify-between">
         <div className="flex flex-col gap-2">
-          <Badge variant="축제">축제</Badge>
-          <p className="text-h2">서울 국제 불꽃 축제</p>
+          <Badge variant="festival">{event.categories}</Badge>
+          <p className="text-h2">{event.name}</p>
         </div>
-        <Button>
+        <Button onClick={onClose}>
           <X />
         </Button>
       </div>
 
       <div className="flex flex-col gap-6 md:flex-row">
-        <EventDetailModalLeft />
-        <EventDetailModalRight />
+        <EventDetailModalLeft event={event} />
+        <EventDetailModalRight event={event} />
       </div>
     </div>
   );
