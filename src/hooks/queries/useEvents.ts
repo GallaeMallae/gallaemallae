dacbbb7cdbfd4020d59b8c2b11259b59f19b8692
GwonWooL <229861@naver.com>
2026-03-19 07:13:55 +0900

    Fix: useEvents 리액트 쿼리 타입 에러 해결 및 스태일 타임 최적화

--- src/hooks/queries/useEvents.ts
+++ src/hooks/queries/useEvents.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchEvents } from "@/lib/api/events";
+import { Event } from "@/types/event";
 
 export const useEvents = () => {
-  return useQuery({
+  return useQuery<Event[]>({
     queryKey: ["events"],
     queryFn: fetchEvents,
-    staleTime: 1000 * 60 * 60, // 축제 데이터는 자주 안 변하니까 1시간 정도 캐싱
+    staleTime: 1000 * 60 * 60, // 1시간 캐싱
   });
 };
