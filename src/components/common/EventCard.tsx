cb1bff55234f132355f454db78b2b639de6daded
GwonWooL <229861@naver.com>
2026-03-19 09:24:03 +0900

    Chore: 불필요한 import 제거

--- src/components/common/EventCard.tsx
+++ src/components/common/EventCard.tsx
@@ -2,7 +2,6 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Heart, Calendar, MapPin } from "lucide-react";
-import { cn } from "@/lib/utils";
 import { formatDateRange } from "@/utils/date";
 import { EventCardItem } from "@/types/common";
 import { CATEGORY_LABELS } from "@/types/common";
