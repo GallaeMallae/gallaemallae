abbd48f44c2f25dc04187ae2395dda477bb4b300
soundraven <kin9709@gmail.com>
2026-03-25 13:20:47 +0900

    Feat: 일정 목록 카드에서 일정 삭제 기능 추가, 일정 수정은 드롭다운 메뉴에 추가해 두었으나 기능은 추후 연결

--- src/components/mypage/MypageAgendaCard.tsx
+++ src/components/mypage/MypageAgendaCard.tsx
@@ -1,28 +1,32 @@
 import { Badge } from "@/components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useDeleteEventPlan } from "@/hooks/mutations/useDeleteEventPlan";
 import { CATEGORY_NAME_MAP } from "@/lib/constants";
 import { MypageDisplayEvent } from "@/types/common";
 import { calculateDDay, formatDate } from "@/utils/date";
+import { MoreVertical, Pencil, Trash } from "lucide-react";
 
 interface MypageAgendaCardProps {
   event: MypageDisplayEvent;
   selectedDate?: string;
-  onClick?: () => void;
 }
 
 export type CategoryKey = keyof typeof CATEGORY_NAME_MAP;
 
-export default function MypageAgendaCard({
-  event,
-  onClick,
-}: MypageAgendaCardProps) {
+export default function MypageAgendaCard({ event }: MypageAgendaCardProps) {
+  const { mutate: deletePlan, isPending: isDeletePlanLoading } =
+    useDeleteEventPlan();
   const formatVisitDate = formatDate(event.display_date);
   const dDay = calculateDDay(event.display_date);
 
   return (
-    <div
-      className="hover:bg-muted flex cursor-pointer flex-col gap-1 rounded-xl p-2"
-      onClick={onClick}
-    >
+    <div className="hover:bg-muted flex cursor-pointer flex-col gap-1 rounded-xl p-2">
       <div className="flex items-center justify-between">
         <div className="flex gap-2">
           {event.categories?.map((category) => {
@@ -40,7 +44,29 @@ export default function MypageAgendaCard({
             );
           })}
         </div>
-        <div className="text-desc2 text-symbol-sky font-semibold">{dDay}</div>
+        <div className="flex items-center gap-2">
+          <div className="text-desc2 text-symbol-sky font-semibold">{dDay}</div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild onClick={(e) => e.stopPropagation()}>
+              <MoreVertical className="size-4" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <Pencil />
+                  일정 수정
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuItem
+                variant="destructive"
+                onClick={() => event.plan_id && deletePlan(event.plan_id)}
+              >
+                <Trash />
+                일정 삭제
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
       <div>
         <div className="text-desc2 truncate font-semibold">{event.name}</div>
