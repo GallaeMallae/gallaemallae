0f9587adad5644767f7164bc3ffdbf62c707f3cc
GwonWooL <229861@naver.com>
2026-03-31 10:41:15 +0900

    Refactor: 중복 구현된 기능 제거

--- src/components/map/EventDetailModal/EventDetailModalRight/EventDetailButton.tsx
+++ src/components/map/EventDetailModal/EventDetailModalRight/EventDetailButton.tsx
@@ -1,22 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Heart } from "lucide-react";
-import { useUserData } from "@/hooks/queries/useUserData";
 
 export default function EventDetailModalButton() {
-  const { data: user } = useUserData();
-
-  const handleClick = () => {
-    if (!user) {
-      alert("로그인이 필요한 기능입니다.");
-    }
-  };
-
   return (
     <div className="flex gap-4">
-      <Button
-        onClick={handleClick}
-        className="bg-symbol-sky text-desc1! flex h-12 flex-1 text-white"
-      >
+      <Button className="bg-symbol-sky text-desc1! flex h-12 flex-1 text-white">
         나의 일정에 추가하기
         <ArrowRight size={20} />
       </Button>
