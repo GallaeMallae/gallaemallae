c25a0f07ece4aedddaa7ceb58f6137d8d21abfb8
GwonWooL <229861@naver.com>
2026-04-10 00:08:01 +0900

    Feat: 중심 캐러셀 시각적 강조 UI 적용

--- src/components/map/Area/EventCarousel.tsx
+++ src/components/map/Area/EventCarousel.tsx
@@ -33,6 +33,7 @@ export default function EventCarousel({
       }
     };
     api.on("select", handleSelect);
+
     return () => {
       api.off("select", handleSelect);
     };
@@ -51,17 +52,28 @@ export default function EventCarousel({
     <div className="absolute right-0 bottom-6 left-0 z-5 px-4">
       <Carousel setApi={setApi} opts={{ align: "center" }}>
         <CarouselContent className="-ml-4">
-          {events.map((event) => (
-            <CarouselItem
-              key={event.id}
-              className="basis-[80%] pl-4 md:basis-86"
-              onClick={() => onSelectCarousel(event.id)}
-            >
-              <div onClick={() => onCarouselClick(event.id)}>
-                <EventCard {...event} />
-              </div>
-            </CarouselItem>
-          ))}
+          {events.map((event) => {
+            const isSelected = selectedId === event.id;
+            return (
+              <CarouselItem
+                key={event.id}
+                className="basis-[80%] pl-4 md:basis-86"
+                onClick={() => onSelectCarousel(event.id)}
+              >
+                <div
+                  className={`overflow-hidden rounded-2xl transition-all duration-300 ${
+                    isSelected
+                      ? "border-symbol-sky scale-105 border-r-4 border-l-4 shadow-xl"
+                      : "scale-95 opacity-60"
+                  } `}
+                >
+                  <div onClick={() => onCarouselClick(event.id)}>
+                    <EventCard {...event} />
+                  </div>
+                </div>
+              </CarouselItem>
+            );
+          })}
         </CarouselContent>
       </Carousel>
     </div>
